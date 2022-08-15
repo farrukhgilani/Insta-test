0d6a6d6b563884c98e654012cc45f9447b055af5
farrukhgilani <sgilani@devsinc.com>
2022-08-15 11:10:03 +0500

    added username validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates: :name, presence: true
-
+  validates: :username, presence: true, uniqueness: true
   has_many :followers, class_name: "Friendship", foreign_key: "follower_id"
   has_many :followed, through: :followers
   has_many :followed, class_name: "Friendship", foreign_key: "followed_id"
