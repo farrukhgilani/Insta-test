0f42565e4487cd909b77140a3ca0d1c00ff69c78
farrukhgilani <sgilani@devsinc.com>
2022-08-15 10:43:57 +0500

    Added user name validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,11 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  validates: :name, presence: true
+
   has_many :followers, class_name: "Friendship", foreign_key: "follower_id"
   has_many :followed, through: :followers
-
   has_many :followed, class_name: "Friendship", foreign_key: "followed_id"
   has_many :followers, through: :followed
-
   has_many :posts
 end
